2ab4901fcc85198ee7694bc243a93350803e2938
cielesan <cielesan.san@gmail.com>
2025-11-19 15:17:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Gunakan port yang disediakan Koyeb atau default 8000
+const port = process.env.PORT || 8000;
+
+// Endpoint root untuk health check
+app.get('/', (req, res) => {
+  res.send('Bot Lan Lan Manager is running!');
+});
+
+// Jalankan server
+app.listen(port, () => {
+  console.log(`HTTP server running on port ${port}`);
+});
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
